48fd9ea4a6a2fe8bb22bedb2b3622f86b2c2b2c2
stepan <stp.eternal@gmail.com>
2012-03-30 19:15:08 +0600

    add .gitignore

--- 1-50/4.rb
+++ 1-50/4.rb
@@ -1,3 +1,12 @@
+# encoding: utf-8
+
+# Problem 4
+# A palindromic number reads the same both ways. 
+# The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+
+# Find the largest palindrome made from the product of two 3-digit numbers.
+
+
 class ::String
   def palindrom?
     return true if self.size < 2
